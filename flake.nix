0bfe8359234c1128e62114bb234da8d48f8a5efe
nulladmin1 <shrey.deogade@protonmail.com>
2025-01-07 19:55:25 -0500

    initial

---
+++ flake.nix
@@ -0,0 +1,52 @@
+{
+  description = "Nix Flake Template for C++";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    systems.url = "github:nix-systems/default";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    systems,
+    ...
+  }: let
+    forEachSystem = nixpkgs.lib.genAttrs (import systems);
+    pkgsFor = forEachSystem (system: import nixpkgs {inherit system;});
+  in {
+    formatter = forEachSystem (system: pkgsFor.${system}.alejandra);
+
+    devShells = forEachSystem (system: {
+      default = pkgsFor.${system}.mkShell {
+        packages = with pkgsFor.${system}; [
+          libllvm
+          cmake
+          gtest
+        ];
+      };
+    });
+
+    packages = forEachSystem (system: {
+      default = pkgsFor.${system}.stdenv.mkDerivation {
+        pname = "cpp";
+        version = "0.1.0";
+        src = ./.;
+
+        nativeBuildInputs = with pkgsFor.${system}; [
+          cmake
+        ];
+        buildInputs = with pkgsFor.${system}; [
+          gtest
+        ];
+      };
+    });
+
+    apps = forEachSystem (system: {
+      default = {
+        type = "app";
+        program = "${self.packages.${system}.default}/bin/hello";
+      };
+    });
+  };
+}
